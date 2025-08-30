f52bc74c09c28dae9d3e055e2dfdc55853d37069
mccreed13 <a.lapushenko13@gmail.com>
2025-08-30 19:28:50 +0300

    Update

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,12 +4,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
-import java.io.File;
-import java.util.Objects;
-import java.util.UUID;
-
-import static org.apache.commons.io.FileUtils.deleteDirectory;
-
 public abstract class BaseTest {
     public void setup(){
         WebDriverManager.chromedriver().browserInDocker().dockerNetworkHost();
@@ -18,13 +12,6 @@ public abstract class BaseTest {
         Configuration.browserSize = "1920x1080";
         Configuration.headless = false;
         Configuration.timeout = 8000;
-        try {
-            String userDataDir = createTempChromeDir();
-            WebDriverManager.chromedriver().properties("--user-data-dir=" + userDataDir);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Chrome props", e);
-        }
     }
 
     @BeforeClass
@@ -36,27 +23,4 @@ public abstract class BaseTest {
     public void test() {
         Selenide.closeWebDriver();
     }
-
-    public static String createTempChromeDir() throws Exception {
-        String baseDir = "/tmp/chrome-tmp/";
-        String dirName = "chrome-tmp-" + UUID.randomUUID();
-        String fullPath = baseDir + dirName;
-        File base = new File(baseDir);
-        for (File file : Objects.requireNonNull(base.listFiles())) {
-            if (file.isDirectory() && file.getName().startsWith("chrome-tmp-")) {
-                deleteDirectory(file); // recursive silme
-            }
-        }
-
-        File dir = new File(fullPath);
-        if (!dir.exists()) {
-            boolean created = dir.mkdirs();
-            if (!created) {
-                throw new RuntimeException("Dizin oluşturulamadı: " + fullPath);
-            }
-        }
-
-        return fullPath;
-    }
-
 }
