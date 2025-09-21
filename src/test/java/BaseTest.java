23bb6ec4203f835c886bc093bfa275d16a28c711
mccreed13 <a.lapushenko13@gmail.com>
2025-09-21 20:48:46 +0300

    Updated with Selenoid

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,17 +1,25 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import java.util.HashMap;
+
 public abstract class BaseTest {
     public void setup(){
-        WebDriverManager.chromedriver().setup();
+        Configuration.remote = "http://localhost:4444/wd/hub";
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
-        Configuration.headless = false;
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("browserVersion", "128.0");
+        options.setCapability("selenoid:options", new HashMap<String, Object>() {{
+            put("enableVideo", true);
+            put("enableVNC", true);
+        }});
+        Configuration.browserCapabilities = options;
         Configuration.timeout = 8000;
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
